9cf9c95b817d7dea60f391c39558781cab6793d5
CristianoXavierxxt <cristianox260@gmail.com>
2023-11-15 12:23:09 -0300

    mais  um teste

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose"
 
-const connectDatabase = () =>{
-    console.log( "Wait connect to the database" )
-
+function connectDatabase() {
     mongoose.connect( process.env.MONGO_DB_URI )
     .then( ()=> console.log( "MongoDB Atlas connected" ) )
     .catch( (error) => console.log(error) )
